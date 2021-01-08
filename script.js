454195b2371f23a7deb884fc94202fa20318300a
tomg <goresthomasj@johndeere.com>
2021-01-08 15:17:34 -0600

    MVP

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 /* 
 MVP requirements
-I'm building a flash card game
+This will be a flash card game
+
+a grid of squares will be presented
+
+the player will choose a random square
+
+the squares will show animals when selected
+
+the player views the image shown and tries to remembers it
+
+another square is selected
+
+if this square shows the same animal then the match is made and the images remain
+
+if the square shows a different image then both images disappear and the player picks again
+
+a message will display when all sqaures are matched to their pair
 
 styling will be minimal
 
-Animations for winning
 */
